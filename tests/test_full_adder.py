622b0ba94cb6d4d67f99c63038d69e6b16da7e39
matheus costa <matheuscosta.tech@gmail.com>
2022-08-08 13:59:24 -0300

    Adding test to full adder

---
+++ tests/test_full_adder.py
@@ -0,0 +1,19 @@
+from ..main import full_adder
+
+def test_full_adder():
+    adder_out = [
+        full_adder(0, 0, 0),
+        full_adder(0, 0, 1),
+        full_adder(0, 1, 0),
+        full_adder(0, 1, 1),
+        full_adder(1, 0, 0),
+        full_adder(1, 0, 1),
+        full_adder(1, 1, 0),
+        full_adder(1, 1, 1),
+    ]
+
+    truth_table_carry_out = [0, 0, 0, 1, 0, 1, 1, 1]
+    truth_table_result = [0, 1, 1, 0, 1, 0, 0, 1]
+    
+    result = list(zip(truth_table_result, truth_table_carry_out))
+    assert adder_out == result
