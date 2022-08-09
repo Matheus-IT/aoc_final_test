8bfa0558e19d1cd038452027bc04e628b97f1e11
matheus costa <matheuscosta.tech@gmail.com>
2022-08-08 22:01:11 -0300

    Implementing sum

--- main.py
+++ main.py
@@ -16,18 +16,24 @@ def ula(a: int, b: int, a_invert: bool, b_invert: bool, carry_in: int):
     b = mux(b, int(not b), b_invert)
     result_and = a and b
     result_or = a or b
-    full_adder(result_and, result_or, carry_in)
+    return full_adder(a, b, carry_in)
 
 
 def main():
+    # Somando 10 mais 1
     a = [1, 0, 1, 0]
     b = [0, 0, 0, 1]
     carry_in = 0
 
-    # for i in range(len(a)):
-    #     result = ula(a[i], b[i], a_invert=False, b_invert=False, carry_in=carry_in)
-    #     print(result)
+    sub_res = []
+    for i in range(len(a)-1, -1, -1):
+        result, carry_out = ula(a[i], b[i], a_invert=False, b_invert=False, carry_in=carry_in)
+        carry_in = carry_out
+        sub_res.append(result)
 
+    sub_res =sub_res[::-1]
+    print(sub_res)
+    
 
 if __name__ == '__main__':
     main()
