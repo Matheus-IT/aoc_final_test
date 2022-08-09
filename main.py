4c4136095d65ae7e11b6dfcde4fad3ae413d19dc
matheus costa <matheuscosta.tech@gmail.com>
2022-08-09 10:24:13 -0300

    Alu returns if was overflow

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from typing import Literal, Union
+
+
 def mux(val: int, inverted: int, use_inverted: bool):
     return inverted if use_inverted else val
 
@@ -9,12 +12,25 @@ def full_adder(a, b, carry_in):
     return result, carry_out
 
 
-def alu(a: int, b: int, a_invert: int, b_invert: int, carry_in: int):
-    a = mux(a, int(not a), a_invert)
-    b = mux(b, int(not b), b_invert)
-    result_and = a and b
-    result_or = a or b
-    return full_adder(a, b, carry_in)
+class Alu:
+    def __init__(self):
+        self.last_calculation_was_overflow: Literal[0, 1] = 0
+    
+    def receive_entries(self, a: int, b: int, a_invert: int, b_invert: int, carry_in: int, is_the_last_bit: bool = False):
+        a = mux(a, int(not a), a_invert)
+        b = mux(b, int(not b), b_invert)
+        result_and = a and b
+        result_or = a or b
+
+        result, carry_out = full_adder(a, b, carry_in)
+        
+        if is_the_last_bit and carry_out == 1:
+            self.last_calculation_was_overflow = 1
+        
+        return result, carry_out
+
+    def overflow(self):
+        return self.last_calculation_was_overflow
 
 
 def main():
--- tests/test_alu.py
+++ tests/test_alu.py
@@ -1,4 +1,4 @@
-from ..main import alu
+from ..main import Alu
 
 def test_ula_sum():
     # Somando 10 mais 1
@@ -6,9 +6,10 @@ def test_ula_sum():
     b = [0, 0, 0, 1]
     carry_in = 0
 
+    alu = Alu()
     sum_res = []
     for i in range(len(a)-1, -1, -1):
-        result, carry_out = alu(a[i], b[i], a_invert=0, b_invert=0, carry_in=carry_in)
+        result, carry_out = alu.receive_entries(a[i], b[i], a_invert=0, b_invert=0, carry_in=carry_in)
         carry_in = carry_out
         sum_res.append(result)
 
@@ -23,10 +24,32 @@ def test_ula_sub():
     b = [0, 0, 0, 1]
     carry_in = 1
     sub_res = []
+    alu = Alu()
     for i in range(len(a)-1, -1, -1):
-        result, carry_out = alu(a[i], b[i], a_invert=0, b_invert=1, carry_in=carry_in)
+        result, carry_out = alu.receive_entries(a[i], b[i], a_invert=0, b_invert=1, carry_in=carry_in)
         carry_in = carry_out
         sub_res.append(result)
     sub_res = list(reversed(sub_res))
     nine = [1, 0, 0, 1]
     assert sub_res == nine
+
+
+def test_ula_overflow():
+    """Somando 8 mais 8 deve dar overflow"""
+    a = [1, 0, 0, 0]
+    b = [1, 0, 0, 0]
+
+    carry_in = 0
+    alu = Alu()
+
+    for i in range(len(a)-1, -1, -1):
+        result, carry_out = alu.receive_entries(
+            a[i],
+            b[i],
+            a_invert=0,
+            b_invert=0,
+            carry_in=carry_in,
+            is_the_last_bit=True if i == 0 else False,
+        )
+        carry_in = carry_out
+    assert alu.overflow() == 1
