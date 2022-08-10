3aca44b5bb30f621d7c2762364df4b661a65c1dc
matheus costa <matheuscosta.tech@gmail.com>
2022-08-09 22:06:33 -0300

    Changing sum and subtract implementations

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from typing import Literal, Union
+from typing import List, Literal, Union
 
 
-def mux(val: int, inverted: int, use_inverted: bool):
+def mux(val: int, inverted: int, use_inverted: int):
     return inverted if use_inverted else val
 
 
@@ -13,10 +13,44 @@ def full_adder(a, b, carry_in):
 
 
 class Alu:
-    def __init__(self):
+    def __init__(self, a: List[int], b: List[int]):
         self.last_calculation_was_overflow: Literal[0, 1] = 0
+        self.n1 = a
+        self.n2 = b
     
-    def receive_entries(self, a: int, b: int, a_invert: int, b_invert: int, carry_in: int, is_the_last_bit: bool = False):
+    def do_sum(self):
+        sum_res = []
+        carry_in = 0
+        for i in range(len(self.n1)-1, -1, -1):
+            result, carry_out = self.process_entries(
+                                    self.n1[i], 
+                                    self.n2[i], 
+                                    a_invert=0, 
+                                    b_invert=0, 
+                                    carry_in=carry_in,
+                                    is_the_last_bit=True if i == 0 else False,
+                                )
+            carry_in = carry_out
+            sum_res.append(result)
+        return list(reversed(sum_res))
+    
+    def subtract(self):
+        sub_res = []
+        carry_in = 1
+        for i in range(len(self.n1)-1, -1, -1):
+            result, carry_out = self.process_entries(
+                                    self.n1[i], 
+                                    self.n2[i], 
+                                    a_invert=0, 
+                                    b_invert=1, 
+                                    carry_in=carry_in,
+                                    is_the_last_bit=True if i == 0 else False,
+                                )
+            carry_in = carry_out
+            sub_res.append(result)
+        return list(reversed(sub_res))
+    
+    def process_entries(self, a: int, b: int, a_invert: int, b_invert: int, carry_in: int, is_the_last_bit: bool = False):
         a = mux(a, int(not a), a_invert)
         b = mux(b, int(not b), b_invert)
         result_and = a and b
--- tests/test_alu.py
+++ tests/test_alu.py
@@ -2,75 +2,30 @@ from ..main import Alu
 
 def test_ula_sum():
     # Somando 10 mais 1
-    a = [1, 0, 1, 0]
-    b = [0, 0, 0, 1]
-    carry_in = 0
-
-    alu = Alu()
-    sum_res = []
-    for i in range(len(a)-1, -1, -1):
-        result, carry_out = alu.receive_entries(a[i], b[i], a_invert=0, b_invert=0, carry_in=carry_in)
-        carry_in = carry_out
-        sum_res.append(result)
-
-    sum_res = list(reversed(sum_res))
+    alu = Alu(a=[1, 0, 1, 0], b=[0, 0, 0, 1])
+    sum_res = alu.do_sum()
     eleven = [1, 0, 1, 1]
     assert sum_res == eleven
 
 
 def test_ula_sub():
     # Subtraindo 10 menos 1
-    a = [1, 0, 1, 0]
-    b = [0, 0, 0, 1]
-    carry_in = 1
     sub_res = []
-    alu = Alu()
-    for i in range(len(a)-1, -1, -1):
-        result, carry_out = alu.receive_entries(a[i], b[i], a_invert=0, b_invert=1, carry_in=carry_in)
-        carry_in = carry_out
-        sub_res.append(result)
-    sub_res = list(reversed(sub_res))
+    alu = Alu(a=[1, 0, 1, 0], b=[0, 0, 0, 1])
+    sub_res = alu.subtract()
     nine = [1, 0, 0, 1]
     assert sub_res == nine
 
 
 def test_ula_should_overflow():
     """Somando 8 mais 8 deve dar overflow"""
-    a = [1, 0, 0, 0]
-    b = [1, 0, 0, 0]
-
-    carry_in = 0
-    alu = Alu()
-
-    for i in range(len(a)-1, -1, -1):
-        result, carry_out = alu.receive_entries(
-            a[i],
-            b[i],
-            a_invert=0,
-            b_invert=0,
-            carry_in=carry_in,
-            is_the_last_bit=True if i == 0 else False,
-        )
-        carry_in = carry_out
+    alu = Alu(a=[1, 0, 0, 0], b=[1, 0, 0, 0])
+    alu.do_sum()
     assert alu.overflow() == 1
 
 
 def test_ula_should_not_overflow():
     """Somando 2 mais 2 NÃO deve dar overflow"""
-    a = [0, 0, 1, 0]
-    b = [0, 0, 1, 0]
-
-    carry_in = 0
-    alu = Alu()
-
-    for i in range(len(a)-1, -1, -1):
-        result, carry_out = alu.receive_entries(
-            a[i],
-            b[i],
-            a_invert=0,
-            b_invert=0,
-            carry_in=carry_in,
-            is_the_last_bit=True if i == 0 else False,
-        )
-        carry_in = carry_out
+    alu = Alu(a=[0, 0, 1, 0], b=[0, 0, 1, 0])
+    alu.do_sum()
     assert alu.overflow() == 0
