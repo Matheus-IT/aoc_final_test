6e4394582a716bd21246ecb8d9ec034b3d004655
matheus costa <matheuscosta.tech@gmail.com>
2022-08-10 10:12:35 -0300

    More tests concerning negative numbers

--- tests/test_alu.py
+++ tests/test_alu.py
@@ -2,15 +2,23 @@ from ..main import Alu
 
 
 def test_ula_sum():
-    # Somando 4 mais 1
+    """Somando 4 mais 1"""
     alu = Alu(a=[0, 1, 0, 0], b=[0, 0, 0, 1])
     sum_res = alu.do_sum()
     five = [0, 1, 0, 1]
     assert sum_res == five
 
 
+def test_ula_sum_2():
+    """Somando -4 mais 2"""
+    alu = Alu(a=[1, 1, 0, 0], b=[0, 0, 1, 0])
+    sum_res = alu.do_sum()
+    minus_two = [1, 1, 1, 0]
+    assert sum_res == minus_two
+
+
 def test_ula_sub():
-    # Subtraindo 4 menos 1
+    """Subtraindo 4 menos 1"""
     sub_res = []
     alu = Alu(a=[0, 1, 0, 0], b=[0, 0, 0, 1])
     sub_res = alu.subtract()
@@ -18,10 +26,25 @@ def test_ula_sub():
     assert sub_res == three
 
 
+def test_ula_sub_2():
+    """Somando -4 menos 2"""
+    alu = Alu(a=[1, 1, 0, 0], b=[0, 0, 1, 0])
+    sub_res = alu.subtract()
+    minus_six = [1, 0, 1, 0]
+    assert sub_res == minus_six
+
+
 def test_ula_should_overflow():
     """Somando 7 mais 1 deve dar overflow"""
     alu = Alu(a=[0, 1, 1, 1], b=[0, 0, 0, 1])
     alu.do_sum()
+    assert alu.overflow() == 1
+
+
+def test_ula_should_overflow_2():
+    """Subtraindo -1 de -8 deve dar overflow"""
+    alu = Alu(a=[1, 0, 0, 0], b=[1, 1, 1, 1])
+    alu.subtract()
     assert alu.overflow() == 1
 
 
